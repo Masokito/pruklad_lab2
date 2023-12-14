3892c895e32024e58ae46d4ea5e209c6aec847dd
Masokito <153779596+Masokito@users.noreply.github.com>
2023-12-13 21:07:14 -0800

    Update Train.java

--- lab2/src/Train.java
+++ lab2/src/Train.java
@@ -3,7 +3,8 @@ import java.util.Date;
     public class Train {
         private String arr_point, num;
         private Date time;
-        private int seats;
+        private int genSeats;
+        private int luxSeats;
 
         String getArrPoint(){
             return arr_point;
@@ -17,10 +18,12 @@ import java.util.Date;
             return time;
         }
 
-        int getSeats(){
-            return seats;
+        int getgenSeats(){
+            return genSeats;
         }
-
+        int getluxSeats(){
+            return luxSeats;
+        }
         void setArrPoint(String a_p){
             this.arr_point = a_p;
         }
@@ -32,24 +35,29 @@ import java.util.Date;
             this.time.setTime(time);
         }
 
-        void setSeats(int seats){
-            this.seats = seats;
+        void setgenSeats(int seats){
+            this.genSeats = seats;
+        }
+        void setgluxSeats(int seats){
+            this.luxSeats = seats;
         }
 
-        public Train(String arr_point,String num , int time, int seats){
+        public Train(String arr_point,String num , int time, int genseats,int luxseats){
             this.arr_point = arr_point;
             this.num = num;
             this.time = new Date(time);
-            this.seats = seats;
+            this.genSeats = genseats;
+            this.luxSeats = luxseats;
         }
 
         @Override
         public String toString(){
         return  "\n------------------------------------------\n"+
-                "Arrival point: " + this.arr_point +
+                "Arrival point: " + this.arr_point + "\n"+
                 "Train number: " + this.num + "\n" +
                 "Departure time: " + this.time + "\n"+
-                "Num of seats: " + this.seats+ "\n" +
+                "Num of general seats: " + this.genSeats+ "\n" +
+                "Num of lux seats: " + this.luxSeats+ "\n" +
                 "------------------------------------------\n";
         }
 
